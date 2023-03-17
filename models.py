fa76c44c9366c8d156e5b6b6f02937954caf9d71
Omonroy36 <monroyorlando.36@gmail.com>
2023-03-17 22:04:30 +0000

    change

--- models.py
+++ models.py
@@ -3,18 +3,18 @@ db = SQLAlchemy()
 
 
 class User(db.Model):
-    __tablename__="user"
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     password = db.Column(db.String(150), nullable=False)
     email = db.Column(db.String(150), nullable=False, unique=True)
     firstname = db.Column(db.String(150), nullable=False)
-    lastname = db.Column(db.String(150), nullable=False)
+    # lastname = db.Column(db.String(150), nullable=False)
 
     def __repr__(self):
         return "<User %r>" % self.email
-    
+
     def serialize(self):
-        return{
+        return {
             "id": self.id,
             "email": self.email,
             "firstname": self.firstname,
